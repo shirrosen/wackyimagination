62427ccdc085b08e4e70a7b334e97a02207915ef
shirrosen <92447898+shirrosen@users.noreply.github.com>
2021-10-13 09:52:48 -0400

    Add files via upload

---
+++ java script.js
@@ -0,0 +1,5 @@
+const sunX = window.innerWidth / 2;
+const sunY = window.innerHeight /2;
+const sun.document.querySelector('.sun');
+sun.style.left = '${sunX}px';
+sun.stlye.top = '${sunY}px'// JavaScript Document
